0dcb0899d7cbcfaf533a51c75bcb8910d3b3cc03
Higor Natã <higor.nfs@outlook.com>
2021-03-25 10:37:09 -0300

    Initial Commit

---
+++ RemoteFunctions/RemoteFunctions.h
@@ -0,0 +1,19 @@
+//
+//  RemoteFunctions.h
+//  RemoteFunctions
+//
+//  Created by Higor Natã on 25/03/21.
+//  Copyright © 2021 Higor Natã. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteFunctions.
+FOUNDATION_EXPORT double RemoteFunctionsVersionNumber;
+
+//! Project version string for RemoteFunctions.
+FOUNDATION_EXPORT const unsigned char RemoteFunctionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteFunctions/PublicHeader.h>
+
+
